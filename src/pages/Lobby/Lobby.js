e78d472fe36bd3c8c4cea9e47b20312ae7cca3fe
dmin12 <davidmin12@gmail.com>
2022-07-10 18:17:07 -0700

    Added onSnapshot functionality to Lobby, now updates with Firestore

--- src/pages/Lobby/Lobby.js
+++ src/pages/Lobby/Lobby.js
@@ -1,27 +1,28 @@
-import React, {useState, useRef, useEffect} from 'react';
-import './Lobby.css';
-import {View} from 'react-native';
-import { useLocation } from 'react-router-dom';
-import { getDocumentData, getAllPlayersInRoom } from '../../firebase/database';
+import React, { useState, useRef, useEffect } from "react";
+import { db } from "../../firebase/config.js";
+import "./Lobby.css";
+import { View } from "react-native";
+import { useLocation } from "react-router-dom";
+import { getDocumentData, getAllPlayersInRoom } from "../../firebase/database";
 import {
-    collection,
-    addDoc,
-    updateDoc,
-    arrayUnion,
-    getDoc,
-    doc,
-    onSnapshot
-  } from "firebase/firestore/lite";
+  collection,
+  addDoc,
+  updateDoc,
+  arrayUnion,
+  getDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore/lite";
 function Lobby() {
-    const {state} = useLocation()
+  const { state } = useLocation();
 
-    const listRef = useRef();
-    const [heightvalue, setHeight] = useState();
-    const [listOfPlayers, setListOfPlayers] = useState([]);
+  const listRef = useRef();
+  const [heightvalue, setHeight] = useState();
+  const [listOfPlayers, setListOfPlayers] = useState([]);
 
-    let list_of_players = null;
+  let list_of_players = null;
 
-    /* async function processData(){
+  /* async function processData(){
         // Data processing
         const player_id = state.player_data
         const room_id = (await getDocumentData("players", player_id)).room_id;
@@ -31,19 +32,26 @@ function Lobby() {
         return list_of_players
     } */
 
-    useEffect(() => {
-        async function processData() {
-            const player_id = state.player_data
-            const room_id = (await getDocumentData("players", player_id)).room_id;
-            console.log("room id: ", room_id)
-            const list_of_players = await getAllPlayersInRoom(room_id);
-            setListOfPlayers(list_of_players);
+  useEffect(() => {
+    async function processData() {
+      const player_id = state.player_data;
+      const room_id = (await getDocumentData("players", player_id)).room_id;
+      console.log("room id: ", room_id);
+      return onSnapshot(doc(db, "rooms", room_id), (doc) => {
+        // returns a promise
+        if (!doc.empty) {
+          // DataFromSnapshot is what ever code you use to get an array of data from
+          // a querySnapshot
+          let dataArray = doc.data().player_ids;
+          setListOfPlayers([...dataArray]);
         }
-        
-        processData();
-    }, []); 
+      });
+    }
 
-    /*const processData = async () => {
+    processData();
+  }, []);
+
+  /*const processData = async () => {
         const player_id = state.player_data
         const room_id = (await getDocumentData("players", player_id)).room_id;
         console.log("room id: ", room_id)
@@ -52,69 +60,63 @@ function Lobby() {
         setListOfPlayers(list_of_players); 
     }; */
 
-    /* (async () => {
+  /* (async () => {
         list_of_players = await processData();
         console.log(list_of_players);
       })()
     
     console.log("async is done"); */
 
-    const getListSize = () => {
-        const newHeight = listRef.current.clientHeight;
-        setHeight(newHeight);
-    }
+  const getListSize = () => {
+    const newHeight = listRef.current.clientHeight;
+    setHeight(newHeight);
+  };
 
-    useEffect(() => {
-        getListSize();
-      }, [listOfPlayers]);
+  useEffect(() => {
+    getListSize();
+  }, [listOfPlayers]);
 
-    useEffect(() => {
-        window.addEventListener("resize", getListSize);
-      }, []);
+  useEffect(() => {
+    window.addEventListener("resize", getListSize);
+  }, []);
 
-    /*useEffect(() => {
+  /*useEffect(() => {
         processData();
     }, [listOfPlayers]); */
 
-    return (
-        <div className="lobby">
-            <div className="invite"> Invite Code </div>
-            <div className="code">
-                <div className="letters"> CODE </div>
-            </div>
-            <div className="players">
-                <div className="notepad"
-                    style={{
-                        height: heightvalue
-                    }}
-                />
-                <ul className="listItemClass" ref={listRef}>
-                    
-                    {listOfPlayers ? 
-                    <div>
-                        {listOfPlayers.toString()}
-                    </div> : <li>A random item</li> }
-
-                    {/* {listOfPlayers.map((item) => (
-                    <li className="item" key={item.id}>
-                        <View style={{flexDirection:'row'}}>
-                        <div className="playerName">{item}</div> 
-                        <div className="status">Ready</div>
-                        </View>
-                    </li>))} */}
-
-                    
-                </ul>
-            </div>
-            <div className="chooseStatus">
-                <View style={{flexDirection: 'row'}}>
-                    <button className="statusButton">Ready</button>
-                    <div className="space"/>
-                    <button className="statusButton">Start</button>
-                </View>
-            </div>
-        </div>
-    );
+  return (
+    <div className="lobby">
+      <div className="invite"> Invite Code </div>
+      <div className="code">
+        <div className="letters"> CODE </div>
+      </div>
+      <div className="players">
+        <div
+          className="notepad"
+          style={{
+            height: heightvalue,
+          }}
+        />
+        <ul className="listItemClass" ref={listRef}>
+          {listOfPlayers.map((item) => (
+            <li className="item" key={item.id}>
+              <View style={{ flexDirection: "row" }}>
+                <div className="playerName">{item}</div>
+                <div className="status">Ready</div>
+              </View>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="chooseStatus">
+        <View style={{ flexDirection: "row" }}>
+          <button className="statusButton">Ready</button>
+          <div className="space" />
+          <button className="statusButton">Start</button>
+        </View>
+      </div>
+    </div>
+  );
 }
 
 export default Lobby;
