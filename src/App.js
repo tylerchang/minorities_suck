d7b59b39be03d82f4ca2991db5eda45f021b6c52
Tyler Chang <tylerchang888@gmail.com>
2023-02-15 17:48:54 -0700

    Merge tyler into master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About';
+import HowToPlay from './pages/HowToPlay/HowToPlay';
+import Lobby from './pages/Lobby/Lobby';
+import Questions from './pages/Questions/Questions'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>Mob Mentality</code>
-        </p>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/howtoplay' exact element={<HowToPlay/>} />
+        <Route path='/about' exact element={<About/>} />
+        <Route path='/lobby' exact element={<Lobby/>} />
+        <Route path='/writequestions' exact element={<Questions/>} />
+      </Routes>
+    </Router>
+    </>
+    
   );
 }
 
